fc58ea0adf739e03835c6322308e56a01fb7a450
CotaGM <cotadiseno@gmail.com>
2024-03-19 13:26:04 +0100

    Ejercicio 1, variables

---
+++ Sprint1/tema2/Nivel1.php
@@ -0,0 +1,15 @@
+<?php
+$num_int = 10;
+$num_doub = 3.14;
+$nombre = "Constanza";
+$valor_bool = true;
+define("constanza", 40);
+
+//ejercicio 1
+echo $saludo;
+echo $num_int, "<br/>", 
+$num_doub, "<br/>", 
+$nombre, "<br/>",
+$valor_bool, "<br/>";
+echo "Mi nombre es ". $nombre. " y tengo ". constanza. " años.";
+?>  
