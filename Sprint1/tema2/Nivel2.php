428ac6cbfa503a696a1dac6c63c999123321becf
CotaGM <cotadiseno@gmail.com>
2024-03-20 11:32:58 +0100

    Ejercicio 2, funcion costo chuches

--- Sprint1/tema2/Nivel2.php
+++ Sprint1/tema2/Nivel2.php
@@ -2,11 +2,25 @@
 //Ejercicio 1
 function costoLlamada($min){
   if ($min < 3 ){
-    echo 10, " centimos";
+    echo 10, " centimos", "<br/>";
   }  
   else{
-    echo 10 + (($min - 3)* 5), " centimos";
+    echo 10 + (($min - 3)* 5), " centimos", "<br/>";
   }
 }
 costoLlamada(10);
+
+//Ejercicio 2
+function chocolate($cantidad){
+  return $cantidad * 1;
+}
+
+function chicle($cantidad){
+   return $cantidad * 0.50;
+}
+
+function caramelos($cantidad){
+    return $cantidad * 1.50;
+}
+echo chocolate(2) + chicle(1) + caramelos(1), "€";
 ?>
