e6e75e46b1492f15dd922107906f18c5d17e5d23
CotaGM <cotadiseno@gmail.com>
2024-03-25 16:09:39 +0100

    Exercise 3, characterDetection function

--- Sprint1/tema3/Nivel1.php
+++ Sprint1/tema3/Nivel1.php
@@ -1,5 +1,27 @@
 <?php
 //Ejercicio 1
-$cadena  = array(10, 20, 30, 40, 50);
-echo $cadena;
-?>
+$cadena_numeros  = array(1, 2, 3, 4, 5);
+foreach($cadena_numeros as $elemento){
+echo $elemento, "<br/>";
+}
+//Ejercicio 2
+$X = array(10, 20, 30, 40, 50, 60);
+echo count($X), "<br/>";
+
+array_splice($X, 5, 1);
+
+echo count($X), "<br/>";
+
+//Ejercicio 3 con for each
+function characterDetection($words_array, $characterX) {
+   foreach($words_array as $words);
+       if (strpos($words, $characterX) == true){
+         return "Caracter encontrado";
+       }
+       else{
+         return "Caracter no encontrado";
+       }
+    }
+$words_array = array("hola", "php");
+echo characterDetection($words_array, "h"),"<br/>";
+?> 
