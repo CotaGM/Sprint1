6b46d4352cda9f665eb91896ea6ccacff079c8ff
CotaGM <cotadiseno@gmail.com>
2024-04-01 17:42:24 +0200

    Level 2, exercise 2

--- Sprint1/tema6/Nivel2/Nivel2.php
+++ Sprint1/tema6/Nivel2/Nivel2.php
@@ -1,17 +1,24 @@
 <?php
 class MagicConstants{
 
-    public function getFile(){
-      return __FILE__;
-    }
+  public function getFile(){
+    return __FILE__;
+  }
 
-    public function getDir(){
-      return __DIR__;
-    }
+  public function getDir(){
+    return __DIR__;
+  }
+   
+  public function __toString(){
+    return "La ruta del archivo de métodos mágicos es el siguiente: <br>". $this -> getFile();
+  }
 }  
-$file_name = new MagicConstants();
+/*$file_name = new MagicConstants();
 echo $file_name -> getFile(). "<br>";
 
 $directory_file = new MagicConstants();
-echo $directory_file -> getDir(). "<br>";
+echo $directory_file -> getDir(). "<br>";*/
+
+$file_name = new MagicConstants();
+echo $file_name;
 ?>
