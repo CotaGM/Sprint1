e2711e58ce7cd4725cfa91eec809253813f1be98
CotaGM <cotadiseno@gmail.com>
2024-06-17 17:00:26 +0200

    Reentrega ejercicios con validacion de datos

--- Sprint1/tema6/Nivel1/Ejercicio1/formulario.php
+++ Sprint1/tema6/Nivel1/Ejercicio1/formulario.php
@@ -1,4 +1,14 @@
 <?php
 
-echo "Bienvenida ". $_GET["name"]. " has iniciado sesion con el email ". $_GET["email"];
+    if (strlen($_GET["name"]) <7  || strlen($_GET["name"]) >12){
+      echo "El usuario tiene que tener una longitud entre 7 y 12 caracteres <br>";
+      $errorUserName = true;
+    }
+    if (!filter_var($_GET["email"], FILTER_VALIDATE_EMAIL)) {
+      echo "Email no válido.";
+      $errorEmail = true;
+    }
+    if(!$errorUserName && !$errorEmail) {
+      echo "Bienvenida ". $_GET["name"]. " has iniciado sesion con el email ". $_GET["email"];
+    }
 ?>
